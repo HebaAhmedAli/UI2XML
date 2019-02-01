f0e66acdea96728465885ccd5fe8954a324ee19b
FeryalHisham <fargalaxy24@gmail.com>
2019-02-01 02:40:33 +0200

    Add CNN Model

--- Model/CNN.py
+++ Model/CNN.py
@@ -1,1 +1,49 @@
+# TODO: Create the cnn layers.
 # TODO: Create the cnn model.
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Activation,Lambda, Permute
+from keras.models import Sequential, Model
+from keras.optimizers import RMSprop
+import tensorflow as tf
+import keras.backend as K
+
+def lamdbda_split(value):
+    
+    x = K.expand_dims(value, 4)
+    #(batch_size,rows,cols,depth,?)
+    x1 = tf.split(x, num_or_size_splits=x.shape[1], axis=1)  #(batch_size,1,cols,depth)
+    x = K.concatenate(x1, 4)        #(batch_size,1,cols,depth,rows)
+    x2 = tf.split(x, num_or_size_splits=x.shape[2], axis=1)  #(batch_size,1,1,depth)
+    x = K.concatenate(x2, 4)        #(batch_size,1,1,depth,rows*col)
+
+    return x
+def createCNN(xInput):
+
+    x = Conv2D(64, (3, 3), padding='valid', activation='relu',data_format="channels_last")(xInput)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Conv2D(128, (3, 3), padding='valid', activation='relu',data_format="channels_last")(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Conv2D(256, (3, 3), padding='valid',data_format="channels_last")(x)
+    x = BatchNormalization(axis = 3)(x)
+    x = Activation('relu')(x)
+    x = Conv2D(256, (3, 3), padding='valid', activation='relu',data_format="channels_last")
+    x = MaxPooling2D(pool_size=(2, 1))(x)
+    x = Conv2D(512, (3, 3), padding='valid',data_format="channels_last")
+    x = BatchNormalization(axis = 3)(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(1, 2))(x)
+    x = Conv2D(512, (3, 3), padding='valid',data_format="channels_last")(x)
+    x = BatchNormalization(axis = 3)(x)
+    x = Activation('relu')(x)
+    x = Lambda(lamdbda_split)(x)
+    x = K.squeeze(x,axis=1)
+    x = K.squeeze(x,axis=2)
+    x = Permute(2,1)(x)
+
+    return x
+
+
+def getTrainedCnnModel(xInput,xOutput):
+        cnnModel = Model(inputs = xInput, outputs = xOutput, name='cnnModel')
+        return cnnModel
+     
+    
