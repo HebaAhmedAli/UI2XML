01ea867bd7e0633f276b173c7758be90e2f67795
HebaAhmedAli <bobaahmed2001@gmail.com>
2019-02-02 16:26:53 -0500

    Adding functions to load dataSet

--- LoadData.py
+++ LoadData.py
@@ -1,40 +1,47 @@
 import Utils
+import Preprocessing
 import Model.Constants as Constants
 import numpy as np
 from keras.utils import to_categorical
 
 # TODO: Add function to load dataSet.
 
-def preprocessData(dataset, vocab):
-    X, Y = zip(*dataset)  
+def loadVocab(vocabPath):
+    keyStrings=set()
+    with open(vocabPath, "r") as ins:
+        for line in ins:
+            keyStrings.update([line[:-1]])
+    keyStrings.update(['<pad>'])
+    keyStrings.update(['\t'])
+    keyStrings.update(['\n'])
+    invVocab = dict(enumerate(sorted(keyStrings)))
+    vocab = {v:k for k,v in invVocab.items()}
+    print("vocabulary length = "+str(len(vocab)))
+    print("inv vocabulary length = "+str(len(invVocab)))
+    return vocab, invVocab
+
+def loadData(imagesPath,xmlPath,vocab):
+    fXml=open(xmlPath)
+    Y=fXml.readlines()
+    Yhot , YhotShiftedLeft = preprocessY(Y,vocab)
+    X=[]
+    for i in range(len(Y)):
+        x=Preprocessing.imageReadAndPreprocessing(imagesPath+str(i)+'.png')
+        X.append(x)
+    print("X shape = "+str(np.array(X).shape))
+    print("Yhot shape = "+str(Yhot.shape))
+    return np.array(X), Yhot , YhotShiftedLeft
+    
+    
+def preprocessY(Y,vocab):
+    #X, Y = zip(*dataset) 
     Y = [Utils.sequenceToIndices(t , vocab) for t in Y]
-    YohShiftedLeft=[Y[i][1:len(Y[i])]+[vocab['<pad>']] for i in range(len(Y))]
-    Yoh = np.array(list(map(lambda x: to_categorical(x, num_classes=len(vocab)), Y)))
-    YohShiftedLeft =np.array(list(map(lambda x: to_categorical(x, num_classes=len(vocab)), YohShiftedLeft)))
-    return X, Yoh , YohShiftedLeft
+    print(Y[0])
+    YhotShiftedLeft=[Y[i][1:len(Y[i])]+[vocab['<pad>']] for i in range(len(Y))]
+    Yhot = np.array(list(map(lambda x: to_categorical(x, num_classes=len(vocab)), Y)))
+    YhotShiftedLeft =np.array(list(map(lambda x: to_categorical(x, num_classes=len(vocab)), YhotShiftedLeft)))
+    return  Yhot , YhotShiftedLeft
 
+vocab,invVocab = loadVocab('data/xml_vocab.txt')
+X, Yhot , YhotShiftedLeft = loadData('data/tryImages/','data/XmlTry.lst',vocab)
 
-# Testing to be used.
-keyStrings=set()
-keyStrings.update(["heba"])
-keyStrings.update(["feryal"])
-keyStrings.update(["fatema"])
-keyStrings.update(['<pad>'])
-keyStrings.update(['\t'])
-keyStrings.update(['\n'])
-
-invVocab = dict(enumerate(sorted(keyStrings)))
-vocab = {v:k for k,v in invVocab.items()}
-'''
-print(keyStrings)
-print(invVocab)
-print(vocab)
-print(Utils.sequenceToIndices("heba fatema <pad>",vocab))
-'''
-#####################.
-'''
-Y=["heba fatema","heba feryal","feryal fatema"]
-Y = [Utils.sequenceToIndices(t , vocab) for t in Y]
-YohShiftedLeft=[Y[i][1:len(Y[i])]+[vocab['<pad>']] for i in range(len(Y))]
-Yoh = np.array(list(map(lambda x: to_categorical(x, num_classes=len(vocab)), Y)))
-'''
--- Preprocessing.py
+++ Preprocessing.py
@@ -1,9 +1,11 @@
 from keras.preprocessing import image
 import Model.Constants as Constants
+import numpy as np
 
 
-def preprocessing(imgPath):
+def imageReadAndPreprocessing(imgPath):
         img = image.load_img(imgPath, target_size = (Constants.IMAGE_SIZE,Constants.IMAGE_SIZE))
-        img = img.astype('float32')
-        img /= 255
+        img = np.array(img,dtype='float32')  
+        img /= 255.
+        #img = np.expand_dims(img, 0)  # Add batch dimension.
         return img
