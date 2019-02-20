528cb41fa2457cdced2f1a8c6ddff4b7c71ce08f
HebaAhmedAli <bobaahmed2001@gmail.com>
2019-02-20 18:11:29 -0500

    Fix a bug in drawContours

--- ComponentsExtraction/BoxesExtraction.py
+++ ComponentsExtraction/BoxesExtraction.py
@@ -29,10 +29,12 @@ def extractBoxes(img):
     allBoxes=[]
     morph=preProcess(img)
     #finding the contours
-    (contours, hierarchy, _) = cv2.findContours(morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)    
+    (contours, hierarchy, _) = cv2.findContours(morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)   
     #contours = np.array(contours).reshape((-1,1,2)).astype(np.int32)
-    cv2.drawContours(img,contours,-1,(0,0,255),2)
+    #cv2.drawContours(img,contours,-1,(0,0,255),2)
     for cnt in contours:
+        cnt = np.array(cnt).reshape((-1,1,2)).astype(np.int32)
+        cv2.drawContours(img,[cnt],0,(0,0,255),2)
         x,y,w,h = cv2.boundingRect(cnt)
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
         allBoxes.append([x, y, w, h])
