1e7f924459a319bf30820a4a85ad6e98d934000b
FeryalHisham <fargalaxy24@gmail.com>
2019-06-15 22:09:53 +0200

    Fix checkYrange.

--- Utils.py
+++ Utils.py
@@ -40,17 +40,17 @@ def iou(boxA, boxB):
         return 0
     
 def checkYrange(boxA,boxB):
-    if (boxA[1]>boxB[1] and boxA[1]<(boxB[1]+boxB[3])) or \
+    if (boxA[1]>=boxB[1] and boxA[1]<(boxB[1]+boxB[3])) or \
     ((boxA[1]+boxA[3])>boxB[1] and (boxA[1]+boxA[3])<(boxB[1]+boxB[3])) or \
-    (boxB[1]>boxA[1] and boxB[1]<(boxA[1]+boxA[3])) or \
+    (boxB[1]>=boxA[1] and boxB[1]<(boxA[1]+boxA[3])) or \
     ((boxB[1]+boxB[3])>boxA[1] and (boxB[1]+boxB[3])<(boxA[1]+boxA[3])):
         return True
     return False
 
 def checkXrange(boxA,boxB):
-    if (boxA[0]>boxB[0] and boxA[0]<(boxB[0]+boxB[2])) or \
+    if (boxA[0]>=boxB[0] and boxA[0]<(boxB[0]+boxB[2])) or \
     ((boxA[0]+boxA[2])>boxB[0] and (boxA[0]+boxA[2])<(boxB[0]+boxB[2])) or \
-    (boxB[0]>boxA[0] and boxB[0]<(boxA[0]+boxA[2])) or \
+    (boxB[0]>=boxA[0] and boxB[0]<(boxA[0]+boxA[2])) or \
     ((boxB[0]+boxB[2])>boxA[0] and (boxB[0]+boxB[2])<(boxA[0]+boxA[2])):
         return True
     return False
