8391ff440f60bd0cf40ccc57bae75f04b27a0787
HebaAhmedAli <bobaahmed2001@gmail.com>
2019-06-24 16:04:44 -0400

    Prepare functions for merging.

--- testExtractComponents.py
+++ testExtractComponents.py
@@ -10,23 +10,13 @@ import numpy as np
 from keras.preprocessing import image
 
 
-vocab,invVocab = LoadDataClassification.loadVocab('data/vocab_classification.txt')
-model = load_model('data/ourModel/UI2XMLclassification245000_98_91.h5') # 150 * 150
-# UI2XMLclassification_224_245000_99_93
-# UI2XMLclassificationAlex_224_245000_99_92 adam with 224 * 224
-# UI2XMLclassification245000_98_91 decay with 150 * 150
-# UI2XMLclassification245000_98_90 adam with 150 * 150
-# UI2XMLclassification245000_97_87 with 64 * 64
 
-imagesPath='data/ScreenShots'
 
-def processSave(subdir, file):
+def processImage(subdir, file,model,invVocab):
     img = cv2.imread(subdir+'/' +file)
     imgCopy = copy.copy(img)
     imgXML = image.load_img(subdir+'/' +file)
     imgXML = np.array(imgXML,dtype='float32')  
-    #img = np.copy(imgXML)
-    #imgCopy = np.copy(imgXML)
     file = file.replace('.jpeg','.jpg')
     boxes, texts ,addedManuallyBool ,predictedComponents= ComponentsExtraction.extractComponentsAndPredict(img,imgCopy,model,invVocab)
     margin = 10
@@ -44,8 +34,12 @@ def processSave(subdir, file):
     if Constants.DEBUG_MODE == True :
         fTo=open(subdir+'/compOutputsAll'+file[:-4]+'/texts.txt', 'w+')
     boxesFiltered,textsFiltered,predictedComponentsFiltered=ComponentsExtraction.filterComponents(boxes, texts ,addedManuallyBool ,predictedComponents,imgCopy,model,invVocab)
-    parentNodesForGui = XmlGeneration.generateXml(boxesFiltered,textsFiltered,predictedComponentsFiltered,imgXML,file[:-5],file[len(file)-5])
-    Constants.mapToGui.update( {file[:-5] : (Constants.boxToGui,Constants.idToGui,Constants.predictedToGui,Constants.xmlFilesToGui,parentNodesForGui)})
+    if file[len(file)-5] == 'D':
+        Constants.DYNAMIC=True
+    else:
+        Constants.DYNAMIC=False
+    parentNodesForGui = XmlGeneration.generateXml(boxesFiltered,textsFiltered,predictedComponentsFiltered,imgXML,file[:-6],file[len(file)-6])
+    Constants.mapToGui.update( {file : (Constants.boxToGui,Constants.idToGui,Constants.predictedToGui,Constants.xmlFilesToGui,parentNodesForGui)})
     #print(Constants.mapToGui)
     #parentNodesForGui = XmlGeneration.updateXml(parentNodesForGui,[[19, 18, 44, 42],[19, 201, 502, 64]],['android.widget.'+"ImageButton",'android.widget.'+"ImageView"],['ImageView_0_0_0','EditText_0_2_0'],imgXML,file[:-5],file[len(file)-5])
     if Constants.DEBUG_MODE == True :
@@ -67,11 +61,45 @@ def processSave(subdir, file):
             j+=1  
         cv2.imwrite(subdir+"/boxOutputs/"+file,img)
 
-Constants.mapToGui = {}
-subdir, dirs, _= next(os.walk(imagesPath))
-for direc in dirs:
-    _,_, files= next(os.walk(subdir+'/'+direc))
+def updateImage(subdir,file,valMapFromGui):
+    imgXML = image.load_img(subdir+'/' +file)
+    imgXML = np.array(imgXML,dtype='float32')  
+    file = file.replace('.jpeg','.jpg')  
+    if file[len(file)-5] == 'D':
+        Constants.DYNAMIC=True
+    else:
+        Constants.DYNAMIC=False
+    parentNodesForGui = XmlGeneration.updateXml(valMapFromGui[4],valMapFromGui[0],valMapFromGui[2],valMapFromGui[1],imgXML,file[:-6],file[len(file)-6])
+    Constants.mapToGui.update( {file : (Constants.boxToGui,Constants.idToGui,Constants.predictedToGui,Constants.xmlFilesToGui,parentNodesForGui)})
+        
+# UI2XMLclassification_224_245000_99_93
+# UI2XMLclassificationAlex_224_245000_99_92 adam with 224 * 224
+# UI2XMLclassification245000_98_91 decay with 150 * 150
+# UI2XMLclassification245000_98_90 adam with 150 * 150
+# UI2XMLclassification245000_97_87 with 64 * 64
+'''
+vocab,invVocab = LoadDataClassification.loadVocab('data/vocab_classification.txt')
+model = load_model('data/ourModel/UI2XMLclassification245000_98_91.h5') # 150 * 150
+imagesPath='data/ScreenShots/ourTest'
+'''
+def processAllImages(imagesPath,model,invVocab):
+    Constants.DIRECTORY = imagesPath+'/output'
+    Constants.mapToGui = {}
+    _,_, files= next(os.walk(imagesPath))
     for file in files:
-        imgPath = os.path.join(subdir, file)
+        imgPath = os.path.join(imagesPath, file)
         if (".png" in imgPath or ".jpeg" in imgPath or ".jpg" in imgPath) and ('._' not in imgPath):
-            processSave(subdir+'/'+direc, file)
+            processImage(imagesPath, file,model,invVocab)
+
+def updateAllImages(imagesPath,mapUpdatedFromGui):
+    Constants.mapToGui = {}
+    for (key, val) in mapUpdatedFromGui.items(): 
+        imgPath = os.path.join(imagesPath, key)
+        if (".png" in imgPath or ".jpeg" in imgPath or ".jpg" in imgPath) and ('._' not in imgPath):
+            updateImage(imagesPath, key,val)
+
+vocab,invVocab = LoadDataClassification.loadVocab('data/vocab_classification.txt')
+model = load_model('data/ourModel/UI2XMLclassification245000_98_91.h5') # 150 * 150
+imagesPath='data/ScreenShots/ourTest'
+
+processAllImages(imagesPath,model,invVocab)
--- testHandDrawingProcessing.py
+++ testHandDrawingProcessing.py
@@ -10,15 +10,12 @@ import Utils
 import io
 from PIL import Image
 
-imagesPath='data/HandDrawn'
 
-def processSave(subdir, file):
-    Constants.DIRECTORY = 'data/HandDrawn/output'
-    Constants.HAND_DRAWN = True
+def processImage(subdir, file):
     xImage = np.array(Utils.genTable(300,300))
+    if not os.path.exists(Constants.DIRECTORY+'/drawable'):
+        os.makedirs(Constants.DIRECTORY+'/drawable')
     Image.fromarray(xImage.astype(np.uint8)).save(Constants.DIRECTORY+'/drawable/'+"pic_x.png")
-    if not os.path.exists(Constants.DIRECTORY):
-        os.makedirs(Constants.DIRECTORY)
     path = subdir+'/' +file
     img = cv2.imread(path)
     imgCopy = copy.copy(img)
@@ -30,10 +27,14 @@ def processSave(subdir, file):
     # TODO: Remove last parameter after testing.
     boxes, boxesTranslated, texts, predictedComponents,myImageBox = ComponentsExtraction.extractComponents(img,imgCopy,img4Txt,file)
     myImage = imgXML[myImageBox[1]:myImageBox[1]+myImageBox[3]+1,myImageBox[0]:myImageBox[0]+myImageBox[2]+1]
-    XmlGeneration.generateXml(boxesTranslated,texts,predictedComponents,myImage,file[:-5],file[len(file)-5])
+    if file[len(file)-5] == 'D':
+        Constants.DYNAMIC=True
+    else:
+        Constants.DYNAMIC=False
+    parentNodesForGui = XmlGeneration.generateXml(boxesTranslated,texts,predictedComponents,myImage,file[:-6],file[len(file)-6])
+    Constants.mapToGui.update( {file : (Constants.boxToGui,Constants.idToGui,Constants.predictedToGui,Constants.xmlFilesToGui,parentNodesForGui)})
     margin = 10
     if Constants.DEBUG_MODE == True :
-        
         if not os.path.exists(subdir+'/compOutputs'+file[:-4]):
             os.makedirs(subdir+'/compOutputs'+file[:-4])
         if not os.path.exists(subdir+'/boxOutputs'):
@@ -52,10 +53,34 @@ def processSave(subdir, file):
             j+=1
         cv2.imwrite(subdir+"/boxOutputs/"+file,img)
 
-subdir, dirs, _= next(os.walk(imagesPath))
-for direc in dirs:
-    _,_, files= next(os.walk(subdir+'/'+direc))
+def updateImage(subdir,file,valMapFromGui):
+    imgXML = image.load_img(subdir+'/' +file)
+    imgXML = np.array(imgXML,dtype='float32')  
+    file = file.replace('.jpeg','.jpg')  
+    if file[len(file)-5] == 'D':
+        Constants.DYNAMIC=True
+    else:
+        Constants.DYNAMIC=False
+    parentNodesForGui = XmlGeneration.updateXml(valMapFromGui[4],valMapFromGui[0],valMapFromGui[2],valMapFromGui[1],imgXML,file[:-6],file[len(file)-6])
+    Constants.mapToGui.update( {file : (Constants.boxToGui,Constants.idToGui,Constants.predictedToGui,Constants.xmlFilesToGui,parentNodesForGui)})
+
+
+def processAllImages(imagesPath):
+    Constants.DIRECTORY = imagesPath+'/output'
+    Constants.mapToGui = {}
+    _,_, files= next(os.walk(imagesPath))
     for file in files:
-        imgPath = os.path.join(subdir, file)
+        imgPath = os.path.join(imagesPath, file)
         if (".png" in imgPath or ".jpeg" in imgPath or ".jpg" in imgPath) and ('._' not in imgPath):
-            processSave(subdir+'/'+direc, file)
+            processImage(imagesPath, file)
+
+def updateAllImages(imagesPath,mapUpdatedFromGui):
+    Constants.mapToGui = {}
+    for (key, val) in mapUpdatedFromGui.items(): 
+        imgPath = os.path.join(imagesPath, key)
+        if (".png" in imgPath or ".jpeg" in imgPath or ".jpg" in imgPath) and ('._' not in imgPath):
+            updateImage(imagesPath, key,val)
+
+imagesPath='data/HandDrawn/ourTest'
+Constants.HAND_DRAWN = True
+processAllImages(imagesPath)
