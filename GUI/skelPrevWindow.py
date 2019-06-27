3813c41d5bbee03cf1ba62baf0a119f74d8c3477
Fatema Sherif <fatemakhalid96@gmail.com>
2019-06-27 10:46:25 +0200

    refine preview window loading

--- GUI/skelPrevWindow.py
+++ GUI/skelPrevWindow.py
@@ -11,56 +11,27 @@ import GUI.activityListItem as activityListItem
 
 class previewWindowSkel(object):
     def setupUi(self, prev):
-        # self.mainHLayout = QtWidgets.QHBoxLayout()
-        # self.mainHWidget = QtWidgets.QWidget()
-        # self.mainHWidget.setLayout(self.mainHLayout)
-
-        # self.tabVLayout = QtWidgets.QVBoxLayout()
-        # self.mainHLayout.addLayout(self.tabVLayout)
-        # self.xmlTabWidget = QtWidgets.QTabWidget()
-
-        # self.activeImageLayout = QtWidgets.QVBoxLayout()
-        # self.mainHLayout.addLayout(self.activeImageLayout)
-        # self.activeImage = QtWidgets.QWidget()
-        # self.activeImage.setLayout(self.activeImageLayout)
-        
-        # self.imageList = QtWidgets.QVBoxLayout()
-        # # self.imageList.setLayout(self.mainHLayout)
-        # self.activitysHLayouts = []
-
-        # firstactivityHLayout = QtWidgets.QHBoxLayout()
-        # self.activitysHLayouts.append(firstactivityHLayout)
-        # self.imageList.addLayout(self.activitysHLayouts[0])
-
-        self.verticalLayoutWidget = QtWidgets.QWidget(prev)
-        # self.verticalLayoutWidget.setGeometry(QtCore.QRect(539, 10, 301, 551))
-        self.verticalLayout = QtWidgets.QVBoxLayout()
-        self.xmlTabs = QtWidgets.QTabWidget(self.verticalLayoutWidget)
-        self.verticalLayout.addWidget(self.xmlTabs)
+        self.xmlTabsverticalLayoutWidget = QtWidgets.QWidget(prev)
+        self.xmlTabsverticalLayout = QtWidgets.QVBoxLayout()
+        self.xmlTabs = QtWidgets.QTabWidget(self.xmlTabsverticalLayoutWidget)
+        self.xmlTabsverticalLayout.addWidget(self.xmlTabs)
         tab = QtWidgets.QWidget()
         self.xmlTabs.addTab(tab, "Hope")
 
-        
-        self.verticalLayoutWidget_2 = QtWidgets.QWidget(prev)
-        # self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(280, 10, 261, 551))
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.activeImgVerticalLayoutWidget = QtWidgets.QWidget(prev)
+        self.activeImgverticalLayout = QtWidgets.QVBoxLayout()
         pixmapimage2 = QtGui.QPixmap("/home/fatema/PycharmProjects/UI2XML/GUI/main.jpg").scaled(300, 600)
-        lab = QtWidgets.QLabel(self.verticalLayoutWidget_2)
-        lab.setPixmap(pixmapimage2)
-        self.verticalLayout_2.addWidget(lab)
+        activeImg = QtWidgets.QLabel(self.activeImgVerticalLayoutWidget)
+        activeImg.setPixmap(pixmapimage2)
+        self.activeImgverticalLayout.addWidget(activeImg)
 
-
-
-        self.verticalLayoutWidget_3 = QtWidgets.QWidget(prev)
-        # self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(9, 10, 271, 551))
-        self.verticalLayout_3 = QtWidgets.QVBoxLayout()
-        self.verticalLayout_4 = QtWidgets.QVBoxLayout()
-        self.scrollArea = QtWidgets.QScrollArea(self.verticalLayoutWidget_3)
+        self.activityListVerticalLayoutWidget = QtWidgets.QWidget(prev)
+        self.listScrolVerticalLayout = QtWidgets.QVBoxLayout()
+        self.scrollArea = QtWidgets.QScrollArea(self.activityListVerticalLayoutWidget)
         self.scrollArea.setWidgetResizable(True)
         self.activitysScrollArea = QtWidgets.QWidget()
-        # self.activitysScrollArea.setGeometry(QtCore.QRect(0, 0, 267, 547))
         self.scrollArea.setWidget(self.activitysScrollArea)
-        self.verticalLayout_3.addWidget(self.scrollArea)
+        self.listScrolVerticalLayout.addWidget(self.scrollArea)
         self.activitysHLayouts = []
 
         firstactivityHLayout = activityListItem.activityListItem()
@@ -69,9 +40,10 @@ class previewWindowSkel(object):
 
         self.activitiesList = QtWidgets.QWidget()
         self.scrollArea.setWidget(self.activitiesList)
-        self.activitiesList.setLayout(self.verticalLayout_4)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.activitiesList.setLayout(self.verticalLayout)
 
         self.mainHLayout = QtWidgets.QHBoxLayout()
-        self.mainHLayout.addLayout(self.verticalLayout_3)
-        self.mainHLayout.addLayout(self.verticalLayout_2)
-        self.mainHLayout.addLayout(self.verticalLayout)
+        self.mainHLayout.addLayout(self.listScrolVerticalLayout)
+        self.mainHLayout.addLayout(self.activeImgverticalLayout)
+        self.mainHLayout.addLayout(self.xmlTabsverticalLayout)
