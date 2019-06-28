d97d38a341af97d9a1111c3a04d9369a5ea3647d
Fatema Sherif <fatemakhalid96@gmail.com>
2019-06-28 21:08:42 +0200

    Dynamically fill the list

--- GUI/activityListItem.py
+++ GUI/activityListItem.py
@@ -1,21 +1,20 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-import GUI
+import GUI.utils as utils
 
 
 
 class activityListItem(QHBoxLayout):
-    def __init__(self):
+    def __init__(self, imgpath, imgName):
         super(activityListItem, self).__init__()
         self.imageBoxLay = QVBoxLayout()
         self.nameBoxLay = QVBoxLayout()
         self.imageLabel = QLabel()
         self.imageNameLine = QLabel()
-        pixmapimage = QPixmap("/home/fatema/PycharmProjects/UI2XML/GUI/main.jpg").scaled(50, 50)
+        pixmapimage = QPixmap(imgpath).scaled(50, 50)
         self.imageLabel.setPixmap(QPixmap(pixmapimage))
-        label = "main.jpg"
-        self.imageNameLine.setText(label)
+        self.imageNameLine.setText(imgName)
         self.imageNameLine.setAlignment(Qt.AlignHCenter)
         self.imageLabel.setAlignment(Qt.AlignHCenter)
         self.imageBoxLay.addWidget(self.imageLabel)
