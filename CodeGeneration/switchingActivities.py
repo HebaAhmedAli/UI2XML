91384d451d5596c49676784b997c8079118a14df
feryalHisham <fargalaxy24@gmail.com>
2019-06-29 18:52:45 +0200

    Add Intent in Java.

--- CodeGeneration/switchingActivities.py
+++ CodeGeneration/switchingActivities.py
@@ -1,8 +1,21 @@
 import Constants
+import fileinput
+import sys
+sys.path.append('../')
+
+
 def addIntentToJava(buttonsToActivities):
-    
-    return
-    
+    for buttonId,fromActivity,toActivity in buttonsToActivities:
+        fromActivity = fromActivity[:-6]
+        fromActivityFile = "../"+Constants.DIRECTORY+'/java/com/example/'+Constants.PROJECT_NAME+"/"+fromActivity.capitalize()+"Activity.java"
+        toActivity = toActivity[:-6]
+        intent = "Intent intent = new Intent("+fromActivity+"Activity.this, "+toActivity+"Activity.class)\n;"+"\tstartActivity(intent);"
+        with open(fromActivityFile, 'r') as file:
+            filedata = file.read()
+        filedata = filedata.replace("// onClick logic"+buttonId, intent)
+        print(filedata)
+        with open(fromActivityFile, 'w') as file:
+            file.write(filedata)
 def addIntentToMainifest(buttonsToActivities):
     fTo=open(Constants.DIRECTORY+'/res/layout/'+'AndroidManifest.xml', 'w+')
 
@@ -10,4 +23,6 @@ def addIntentToMainifest(buttonsToActivities):
 
 def switchActivities(buttonsToActivities):
     addIntentToJava(buttonsToActivities)
-    addIntentToMainifest(buttonsToActivities)
+    #addIntentToMainifest(buttonsToActivities)
+
+switchActivities([['0_5_0',"face1AD.jpg","face2AD.jpg"]])
