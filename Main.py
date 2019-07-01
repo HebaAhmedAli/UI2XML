4824b5d6ab524a7e74d8bbbc6594158e02b74b87
Fatema Sherif <fatemakhalid96@gmail.com>
2019-07-01 02:31:46 +0200

    list trial without model

--- Main.py
+++ Main.py
@@ -7,11 +7,11 @@ from GUI.skelChangeCompPred import correctPredDialog
 from GUI.createProjDialog import createProjectDialog
 import  GUI.prevWindow as prevWindow
 import  GUI.utils as utils
-from keras.models import load_model
-import ScreenShots
-import HandDrawing
-import Psd
-import LoadDataClassification
+# from keras.models import load_model
+# import ScreenShots
+# import HandDrawing
+# import Psd
+# import LoadDataClassification
 import Constants
 
 class mainScreen (QMainWindow, skelMainscreen.Ui_mainWindow):
@@ -38,15 +38,15 @@ class mainScreen (QMainWindow, skelMainscreen.Ui_mainWindow):
     def processImagesAccToMode(self):
         del self.mainDialoge
         self.uploadWidget.populateProjDir()
-        if Constants.designMode == Constants.DESIGN_MODES[0]:
-            vocab,invVocab = LoadDataClassification.loadVocab('data/vocab_classification.txt')
-            model = load_model('data/ourModel/'+Constants.MODEL_NAME) # 150 * 150
-            ScreenShots.processAllImages(Constants.imagesPath,model,invVocab)
-        elif Constants.designMode == Constants.DESIGN_MODES[1]:
-            print("hhhh ",Constants.designMode)
-            HandDrawing.processAllImages(Constants.imagesPath)
-        else:  
-            Psd.processAllPsds(Constants.imagesPath,model,invVocab)
+        # if Constants.designMode == Constants.DESIGN_MODES[0]:
+        #     vocab,invVocab = LoadDataClassification.loadVocab('data/vocab_classification.txt')
+        #     model = load_model('data/ourModel/'+Constants.MODEL_NAME) # 150 * 150
+        #     ScreenShots.processAllImages(Constants.imagesPath,model,invVocab)
+        # elif Constants.designMode == Constants.DESIGN_MODES[1]:
+        #     print("hhhh ",Constants.designMode)
+        #     HandDrawing.processAllImages(Constants.imagesPath)
+        # else:  
+        #     Psd.processAllPsds(Constants.imagesPath,model,invVocab)
         prev = prevWindow.previewWindow()
         self.uploadWidget.layoutScroll.addLayout(prev.mainHLayout)
 
