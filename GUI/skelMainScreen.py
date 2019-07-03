cbd0c10293293b2bc96fd99b0cb93397411517af
aabdosobhy <aabdosobhy@gmail.com>
2019-07-04 00:55:03 +0200

    remove new project for fatema

--- GUI/skelMainScreen.py
+++ GUI/skelMainScreen.py
@@ -2,18 +2,20 @@
 
 # Form implementation generated from reading ui file 'AppSkelton.ui'
 #
-# Created by: PyQt5 UI code generator 5.6
+# Created by: PyQt5 UI code generator 5.12.3
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_mainWindow(object):
     def setupUi(self, mainWindow):
         mainWindow.setObjectName("mainWindow")
         mainWindow.resize(800, 600)
-        # mainWindow.setStyleSheet("background-color:\"grey\";")
         self.centralwidget = QtWidgets.QWidget(mainWindow)
+        self.centralwidget.setObjectName("centralwidget")
         mainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(mainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
@@ -37,26 +39,21 @@ class Ui_mainWindow(object):
         self.toolBar.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
         self.toolBar.setObjectName("toolBar")
         mainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
-        self.actionNew = QtWidgets.QAction(mainWindow)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("Resources/Images/new.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.actionNew.setIcon(icon)
-        self.actionNew.setObjectName("actionNew")
         self.actionExit = QtWidgets.QAction(mainWindow)
         icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("Resources/Images/exit.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon1.addPixmap(QtGui.QPixmap("Resources/Images/exit-white.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionExit.setIcon(icon1)
         self.actionExit.setObjectName("actionExit")
         self.actionGenerateXML = QtWidgets.QAction(mainWindow)
         self.actionGenerateXML.setEnabled(False)
         icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap("Resources/Images/run.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon2.addPixmap(QtGui.QPixmap("Resources/Images/run-white.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionGenerateXML.setIcon(icon2)
         self.actionGenerateXML.setObjectName("actionGenerateXML")
         self.actionConnectCmpts = QtWidgets.QAction(mainWindow)
         self.actionConnectCmpts.setEnabled(False)
         icon3 = QtGui.QIcon()
-        icon3.addPixmap(QtGui.QPixmap("Resources/Images/connect.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon3.addPixmap(QtGui.QPixmap("Resources/Images/connect-white.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionConnectCmpts.setIcon(icon3)
         self.actionConnectCmpts.setObjectName("actionConnectCmpts")
         self.actionShow_Results = QtWidgets.QAction(mainWindow)
@@ -77,18 +74,17 @@ class Ui_mainWindow(object):
         self.actionUpdateCmpts = QtWidgets.QAction(mainWindow)
         self.actionUpdateCmpts.setEnabled(False)
         icon4 = QtGui.QIcon()
-        icon4.addPixmap(QtGui.QPixmap("Resources/Images/update.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon4.addPixmap(QtGui.QPixmap("Resources/Images/update-white.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionUpdateCmpts.setIcon(icon4)
         self.actionUpdateCmpts.setObjectName("actionUpdateCmpts")
         self.actionFinish = QtWidgets.QAction(mainWindow)
         self.actionFinish.setEnabled(False)
         icon5 = QtGui.QIcon()
-        icon5.addPixmap(QtGui.QPixmap("Resources/Images/finish.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon5.addPixmap(QtGui.QPixmap("Resources/Images/finish-white.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionFinish.setIcon(icon5)
         self.actionFinish.setObjectName("actionFinish")
         self.actionView_Doccument = QtWidgets.QAction(mainWindow)
         self.actionView_Doccument.setObjectName("actionView_Doccument")
-        self.menuFile.addAction(self.actionNew)
         self.menuFile.addSeparator()
         self.menuFile.addAction(self.actionExit)
         self.menuImage_Grid.addSeparator()
@@ -108,12 +104,11 @@ class Ui_mainWindow(object):
         self.menubar.addAction(self.menuView.menuAction())
         self.menubar.addAction(self.menuProject.menuAction())
         self.menubar.addAction(self.menuHelp.menuAction())
-        self.toolBar.addAction(self.actionNew)
         self.toolBar.addAction(self.actionExit)
         self.toolBar.addSeparator()
         self.toolBar.addAction(self.actionGenerateXML)
-        self.toolBar.addAction(self.actionConnectCmpts)
         self.toolBar.addAction(self.actionUpdateCmpts)
+        self.toolBar.addAction(self.actionConnectCmpts)
         self.toolBar.addAction(self.actionFinish)
 
         self.retranslateUi(mainWindow)
@@ -128,8 +123,7 @@ class Ui_mainWindow(object):
         self.menuHelp.setTitle(_translate("mainWindow", "Help"))
         self.menuProject.setTitle(_translate("mainWindow", "Run"))
         self.toolBar.setWindowTitle(_translate("mainWindow", "toolBar"))
-        self.actionNew.setText(_translate("mainWindow", "New"))
-        self.actionNew.setShortcut(_translate("mainWindow", "Ctrl+N"))
+
         self.actionExit.setText(_translate("mainWindow", "Exit"))
         self.actionGenerateXML.setText(_translate("mainWindow", "Generate XML"))
         self.actionGenerateXML.setShortcut(_translate("mainWindow", "F5"))
