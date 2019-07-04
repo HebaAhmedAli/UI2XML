6478f55fe137c0389b24292197a607c1992fce27
HebaAhmedAli <bobaahmed2001@gmail.com>
2019-07-04 11:22:57 +0200

    Revert last merge.

--- UI2XML/ScreenShotMode/TextExtraction.py
+++
@@ -1,9 +0,0 @@
-import pytesseract as pt
-
-# Extract text from given image or box.
-def extractText(croppedImage):
-    textExtracted=""
-    #TODO: (we may need to merge the extracted text before return it)
-    textExtracted = pt.image_to_string(croppedImage)
-    return textExtracted
-    
