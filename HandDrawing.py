72b1727b188539ab45e0dc6b93ac7203ba6c1031
HebaAhmedAli <bobaahmed2001@gmail.com>
2019-07-05 16:01:50 +0200

    Handle handDrawn no parrallization.

--- HandDrawing.py
+++ HandDrawing.py
@@ -1,5 +1,4 @@
 import HandDrawingMode.ComponentsExtraction as ComponentsExtraction
-from multiprocessing import Process,Manager
 from keras.preprocessing import image
 import CodeGeneration.XmlGeneration as XmlGeneration
 import numpy as np
@@ -13,7 +12,21 @@ from PIL import Image
 import time
 
 
-def constructXml(subdir,file,img,imgCopy,imgXML,myImageBox,boxes,boxesTranslated,texts,predictedComponents):
+def processImage(subdir, file):
+    xImage = np.array(Utils.genTable(300,300))
+    if not os.path.exists(Constants.DIRECTORY+'/res/drawable'):
+        os.makedirs(Constants.DIRECTORY+'/res/drawable')
+    Image.fromarray(xImage.astype(np.uint8)).save(Constants.DIRECTORY+'/res/drawable/'+"pic_x.png")
+    path = subdir+'/' +file
+    img = cv2.imread(path)
+    imgCopy = copy.copy(img)
+    imgXML = image.load_img(subdir+'/' +file)
+    imgXML = np.array(imgXML,dtype='float32')
+    with io.open(path, 'rb') as image_file:
+        img4Txt = image_file.read()
+    file = file.replace('.jpeg','.jpg')
+    # TODO: Remove last parameter after testing.
+    boxes, boxesTranslated, texts, predictedComponents,myImageBox = ComponentsExtraction.extractComponents(img,imgCopy,img4Txt,file)
     myImage = imgXML[myImageBox[1]:myImageBox[1]+myImageBox[3]+1,myImageBox[0]:myImageBox[0]+myImageBox[2]+1]
     boxToGui=[]
     predictedToGui=[]
@@ -47,49 +60,17 @@ def constructXml(subdir,file,img,imgCopy,imgXML,myImageBox,boxes,boxesTranslated
             j+=1
         cv2.imwrite(subdir+"/boxOutputs/"+file,img)
 
-
-def createProcess(subdir,file,img,imgCopy,imgXML,myImageBox,boxes,boxesTranslated,texts,predictedComponents):
-    process = Process(target=constructXml, args=(subdir,file,img,imgCopy,imgXML,myImageBox,boxes,boxesTranslated,texts,predictedComponents))
-    return process 
-
-def processImage(subdir, file):
-    xImage = np.array(Utils.genTable(300,300))
-    if not os.path.exists(Constants.DIRECTORY+'/res/drawable'):
-        os.makedirs(Constants.DIRECTORY+'/res/drawable')
-    Image.fromarray(xImage.astype(np.uint8)).save(Constants.DIRECTORY+'/res/drawable/'+"pic_x.png")
-    path = subdir+'/' +file
-    img = cv2.imread(path)
-    imgCopy = copy.copy(img)
-    imgXML = image.load_img(subdir+'/' +file)
-    imgXML = np.array(imgXML,dtype='float32')
-    with io.open(path, 'rb') as image_file:
-        img4Txt = image_file.read()
-    file = file.replace('.jpeg','.jpg')
-    # TODO: Remove last parameter after testing.
-    boxes, boxesTranslated, texts, predictedComponents,myImageBox = ComponentsExtraction.extractComponents(img,imgCopy,img4Txt,file)
-    return createProcess(subdir,file,img,imgCopy,imgXML,myImageBox,boxes,boxesTranslated,texts,predictedComponents)   
-
-
-
 def processAllImages(imagesPath):
     startTime = time.time()
     Constants.HAND_DRAWN = True
     Constants.DIRECTORY = imagesPath[:-5] + Constants.androidPath
-    manager=Manager()
     # Initialize the vectors of each image with empty vector(this vector is shared between processes)
-    Constants.mapToGui=manager.dict()
-    processes = []
+    Constants.mapToGui={}
     _,_, files= next(os.walk(imagesPath))
     for file in files:
         imgPath = os.path.join(imagesPath, file)
         if (".png" in imgPath or ".jpeg" in imgPath or ".jpg" in imgPath) and ('._' not in imgPath):
-            process = processImage(imagesPath, file)
-            processes.append(process)
-    for p in processes:
-        p.start()
-    for p in processes:
-        p.join()
-        p.terminate()
+            processImage(imagesPath, file)
     print("Total time = ",time.time()-startTime)
 
 def updateImage(subdir,file,valMapFromGui):
@@ -113,28 +94,15 @@ def updateImage(subdir,file,valMapFromGui):
     Constants.mapToGui.update( {file: [boxToGui,idToGui,predictedToGui,xmlFilesToGui,inWhichFile,parentNodesForGui,valMapFromGui[4]]})
 
 
-def createUpdateProcess(subdir,file,valMapFromGui):
-    process = Process(target=updateImage, args=(subdir,file,valMapFromGui))
-    return process    
-
-
-
 def updateAllImages(imagesPath,mapUpdatedFromGui):
     Constants.HAND_DRAWN = True
-    manager=Manager()
     # Initialize the vectors of each image with empty vector(this vector is shared between processes)
-    Constants.mapToGui=manager.dict()
-    processes = []
+    Constants.mapToGui={}
     for (key, val) in mapUpdatedFromGui.items(): 
         imgPath = os.path.join(imagesPath, key)
         if (".png" in imgPath or ".jpeg" in imgPath or ".jpg" in imgPath) and ('._' not in imgPath):
-            process = createUpdateProcess(imagesPath,key,val)
-            processes.append(process)
-    for p in processes:
-        p.start()
-    for p in processes:
-        p.join()
-        p.terminate()
+            updateImage(imagesPath,key,val)
+            
 
 '''
 imagesPath='data/HandDrawn/ourTest'
