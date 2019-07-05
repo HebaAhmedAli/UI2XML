d2b65b790adfb973631e2432930b35b6e63a30a3
Fatema Sherif <fatemakhalid96@gmail.com>
2019-07-05 20:21:32 +0200

    Edit Buffering screen.

--- GUI/modelLoading.py
+++ GUI/modelLoading.py
@@ -14,7 +14,9 @@ class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(20, 30, 231, 51))
+        self.label.setGeometry(QtCore.QRect(140, 140, 231, 51))
+        Dialog.setStyleSheet("""background-color:#515781;""")
+        
         font = QtGui.QFont()
         font.setFamily("URW Gothic L")
         font.setPointSize(26)
@@ -23,7 +25,7 @@ class Ui_Dialog(object):
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(80, 80, 600, 70))
+        self.label_2.setGeometry(QtCore.QRect(240, 180, 600, 70))
         font = QtGui.QFont()
         font.setFamily("URW Gothic L")
         font.setPointSize(26)
@@ -31,10 +33,12 @@ class Ui_Dialog(object):
         font.setWeight(75)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
-        self.progressBar = QtWidgets.QProgressBar(Dialog)
-        self.progressBar.setGeometry(QtCore.QRect(80, 400, 800, 31))
-        self.progressBar.setRange(0, 100)
-        self.progressBar.setObjectName("progressBar")
+
+        self._gif = QtWidgets.QLabel(Dialog)
+        movie = QtGui.QMovie("Resources/Images/load.gif")
+        self._gif.setMovie(movie)
+        self._gif.move(400, 300)
+        movie.start()
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
